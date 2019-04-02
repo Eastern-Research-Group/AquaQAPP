3cd30abb7f3aa3fea04c8d707925f972dca23aab
RRenteria <renzo.renteria@erg.com>
2019-04-02 17:18:08 -0400

    SideNav component

---
+++ client/src/components/SideNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <b-modal
+      id="modalPrevent"
+      ref="modal"
+      title="AquaQapp"
+      @ok="handleOk"
+      @shown="handleShown"
+  >
+    <slot/>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'SideNav',
+  props: {
+    handleOk: {
+      type: Function,
+      required: true,
+    },
+    handleShown: {
+      type: Function,
+      required: false,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style scoped>
+#modalPrevent {
+  transition: none !important;
+}
+</style>
+
+<style>
+.modal-dialog {
+  position: fixed;
+}
+
+.modal-content {
+  position: fixed;
+  width: 25em;
+  right: 0;
+  top: 0;
+  border-radius: 0;
+  height: 100%;
+  background-color: #162A49;
+  color: white !important;
+}
+
+.close {
+  color: white !important;
+}
+</style>
