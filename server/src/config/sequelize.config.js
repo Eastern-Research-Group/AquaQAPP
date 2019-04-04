c5ade091ad7b8d78d2da10cd7ead1fad9d299949
dpgalloway <devin.galloway@erg.com>
2019-04-04 16:13:04 -0600

    Fix heroku build issue (need to set sequelize to prod config)

--- server/src/config/sequelize.config.js
+++ server/src/config/sequelize.config.js
@@ -1,19 +1,16 @@
 const config = require('./config');
 
-const devConfig = process.env.DATABASE_URL
-  ? {
-      url: process.env.DATABASE_URL,
-      dialect: 'postgres',
-    }
-  : {
-      username: config.db.user,
-      password: config.db.password,
-      database: config.db.database,
-      host: config.db.options.host,
-      port: config.db.options.port,
-      dialect: config.db.options.dialect,
-    };
-
 module.exports = {
-  development: devConfig,
+  development: {
+    username: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+    host: config.db.options.host,
+    port: config.db.options.port,
+    dialect: config.db.options.dialect,
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
 };
