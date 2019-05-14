85eae4290d48d4966b894d7ff59a2989b37bb64a
RRenteria <renzo.renteria@erg.com>
2019-05-14 14:40:14 -0400

    Parameters

--- client/src/views/Navigate.vue
+++ client/src/views/Navigate.vue
@@ -37,11 +37,10 @@
           :key="question.id"
         >
           <div class="field" v-if="question.questionLabel === 'Water Quality Concerns'">
-            <label id="waterQualityConcerns" class="label is-size-4">{{ question.questionLabel }}</label>
             <Concerns />
           </div>
-          <div class="field" v-if="question.questionLabel === 'Pollutants'">
-            To be developed
+          <div class="field parameters" v-if="question.questionLabel === 'Pollutants'">
+            <Parameters />
           </div>
           <div v-else>
             <label class="label is-size-4">{{ question.questionLabel }}</label>
@@ -121,9 +120,10 @@ import ExampleModal from '@/components/shared/ExampleModal';
 import Tabs from '@/components/shared/Tabs';
 import MarkComplete from '@/components/shared/MarkComplete';
 import Concerns from '@/components/app/Concerns';
+import Parameters from '@/components/app/Parameters';
 
 export default {
-  components: { Locations, Tip, Button, ExampleModal, Tabs, MarkComplete, Concerns },
+  components: { Locations, Tip, Button, ExampleModal, Tabs, MarkComplete, Concerns, Parameters },
   data() {
     return {
       currentOutlineNum: '1',
@@ -268,11 +268,7 @@ textarea {
   margin-left: 0.5em;
 }
 
-.label:not(:last-child) {
-  display: none !important;
-}
-
-#waterQualityConcerns {
-  display: block !important;
+.parameters {
+  display: inline-flex !important;
 }
 </style>
