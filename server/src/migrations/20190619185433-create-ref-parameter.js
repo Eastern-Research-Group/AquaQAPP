2152bf83ed0aef3c9495d82e8e7177ae34de954b
dpgalloway <devin.galloway@erg.com>
2019-06-19 14:15:25 -0600

    Add Parameters.csv to pull in parameters to DB

---
+++ server/src/migrations/20190619185433-create-ref-parameter.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('RefParameters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      concern: {
+        type: Sequelize.STRING,
+      },
+      waterType: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('RefParameters');
+  },
+};
