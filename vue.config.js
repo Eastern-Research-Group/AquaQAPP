360c36a2bd5563e52c763ed3c3c9dc99999d3f6c
Devin Galloway <Devin.Galloway@erg.com>
2019-10-02 09:27:57 -0600

    Make sure Vue CLI copies all necessary files when building for prod, to remove all console errors

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   runtimeCompiler: true,
   outputDir: `${__dirname}/client/dist`,
@@ -11,10 +14,22 @@ module.exports = {
       app: './client/src/main.js',
     },
     devtool: 'source-map',
+    plugins: [
+      // Make sure all files are copied from public to dist (required since our vue-cli directory is nested inside client)
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, 'client/public'),
+          to: path.join(__dirname, 'client/dist'),
+          toType: 'dir',
+          ignore: ['index.html', '.DS_Store'],
+        },
+      ]),
+    ],
   },
   devServer: {
     host: 'localhost',
   },
+  // Make sure client/public/index.html is used for main template
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].template = `${__dirname}/client/public/index.html`;
