82d49090b8f62e9d50a125beb7f8de10936d5e7b
Tyler Goulski <TGoulski@TGoulski-MacBook-Pro.local>
2020-06-12 10:07:52 -0600

    Updated names in table and side-nav in Record Handling Procedures

--- server/src/seeders/ref-procedure.js
+++ server/src/seeders/ref-procedure.js
@@ -2,28 +2,28 @@ module.exports = {
   up: (queryInterface) => {
     const records = [];
     records.push({
-      activity: 'Document and record-keeping',
+      activity: 'Document Control',
       placeholder: 'Indicate where records (forms) will be stored and the length of time they will be retained.',
       procedureSort: 1,
     });
     records.push({
-      activity: 'Data generation',
+      activity: 'Data Generation',
       placeholder: 'Summarize records that will be produced – field, lab, and/or assessment.',
       procedureSort: 2,
     });
     records.push({
-      activity: 'Data quality package',
+      activity: 'Data Quality Report',
       placeholder:
         'Describe final project QAPP including attachments and corrective actions, documentation of any substantive changes made to the AquaQAPP-generated QAPP, formatted data, data evaluation summary.',
       procedureSort: 3,
     });
     records.push({
-      activity: 'Data reporting format',
+      activity: 'Reporting Format',
       placeholder: 'Identify the file type(s) that will be used to deliver data to users (e.g., *.csv, *.xml, *.kml).',
       procedureSort: 4,
     });
     records.push({
-      activity: 'Data storage',
+      activity: 'Records Storage',
       placeholder: 'Location of digital data storage (e.g., EPA’s Water Quality Exchange (WQX) database).',
       procedureSort: 5,
     });
