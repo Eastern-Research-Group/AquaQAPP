b4a87866deb38e38389b715959fc844c89305aec
Devin <devin.galloway@erg.com>
2021-03-04 12:37:43 -0700

    When removing params by location, remove any associated sampling design records

--- client/src/components/app/ParametersByLocation.vue
+++ client/src/components/app/ParametersByLocation.vue
@@ -83,6 +83,7 @@ import unsavedChanges from '@/mixins/unsavedChanges';
 import Button from '@/components/shared/Button';
 import SideNav from '@/components/shared/SideNav';
 import Table from '@/components/shared/Table';
+import difference from 'lodash/difference';
 import sortBy from 'lodash/sortBy';
 
 export default {
@@ -255,6 +256,39 @@ export default {
       this.pendingData = {};
     },
     async editData() {
+      // Before saving, need to check if there are existing sample design detail records for the same location/param combination for the parameters to be removed
+      // If so, delete the sample design detail records before the parameters are removed
+      const removedParams = difference(
+        this.selectedLocation['Parameters By Location'].split(','),
+        this.pendingData.parametersByLocation.split(',')
+      );
+      const valueIdsToDelete = [];
+      removedParams.forEach((paramId) => {
+        const sampleParameters = this.qappData.sampleParameter.filter((p) => p.value === paramId);
+        if (sampleParameters.length) {
+          const sampleLocationObject = this.qappData.sampleLocationId.find(
+            (datum) =>
+              datum.value === this.selectedLocation['Location ID'] &&
+              sampleParameters.map((p) => p.valueId).includes(datum.valueId)
+          );
+          // If parameter and location combo is found in sample design data, add to valueIdsToDelete
+          if (sampleLocationObject) {
+            valueIdsToDelete.push(sampleLocationObject.valueId);
+          }
+        }
+      });
+      // Complete delete action if there are sample design records to delete
+      if (valueIdsToDelete.length) {
+        const samplingDesignQuestions = this.$store.state.structure.questions.filter(
+          (q) => q.section.sectionLabel === 'Sampling Design Details'
+        );
+        await this.$store.dispatch('qapp/deleteData', {
+          qappId: this.$store.state.qapp.id,
+          valueIds: [valueIdsToDelete],
+          questionIds: [samplingDesignQuestions.map((q) => q.id)],
+        });
+      }
+
       await this.$store.dispatch('qapp/updateData', {
         qappId: this.qappId,
         valueId: this.selectedLocation.valueId,
