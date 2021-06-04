65b25f8fe616402f6c95a55c02e37022851cf5af
Devin <devin.galloway@erg.com>
2021-06-04 12:35:27 -0600

    Update sequelize config ssl options

--- server/src/config/sequelize.config.js
+++ server/src/config/sequelize.config.js
@@ -13,7 +13,10 @@ module.exports = {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
